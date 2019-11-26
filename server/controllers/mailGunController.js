8e8d49313213e08b62284bdba6695eec0e78dc3f
Jonathan Cardoz <jcardoz@deloitte.com>
2019-11-27 01:44:21 +0530

    fix: commenting on the controller files

--- server/controllers/mailGunController.js
+++ server/controllers/mailGunController.js
@@ -1,22 +1,22 @@
 exports.mail = (request, response) => {
-    const mailgun = require("mailgun-js");
-    const DOMAIN = process.env.MAILGRID_DOMAIN
-    const mg = mailgun({
-        apiKey: process.env.MAILGRID_API_KEY,
-        domain: DOMAIN
-    });
-    const data = {
-        from: request.params.from,
-        to: request.params.to,
-        cc: request.params.cc,
-        bcc: request.params.bcc,
-        subject: request.params.subject,
-        text: request.params.content
-    };
-    mg.messages().send(data, function (error, body) {
-        response.json(body);
-    },(err) => {
-      response.send('Something went wrong. '+err.message);
-    });
+  const mailgun = require("mailgun-js");
+  const DOMAIN = process.env.MAILGRID_DOMAIN
+  const mg = mailgun({
+    apiKey: process.env.MAILGRID_API_KEY,
+    domain: DOMAIN
+  });
+  const data = {
+    from: request.params.from,
+    to: request.params.to,
+    cc: request.params.cc,
+    bcc: request.params.bcc,
+    subject: request.params.subject,
+    text: request.params.content
+  };
+  mg.messages().send(data, function (error, body) {
+    response.json(body);
+  }, (error) => {
+    response.send(`Something went wrong. ${error.message}`);
+  });
 
 };
--- server/controllers/sendGridController.js
+++ server/controllers/sendGridController.js
@@ -1,20 +1,20 @@
 exports.mail = (request, response) => {
-    const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    // process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; // TODO: Hack to run without certificate error
-    const msg = {
-        to: request.params.to.split(','),
-        cc: request.params.cc.split(','),
-        bcc: request.params.bcc.split(','),
-        from: request.params.from,
-        subject: request.params.subject,
-        text: request.params.content,
-        html: request.params.content,
-    };
-    sgMail.send(msg).then((body, error) => {
-        response.json(body);
-    }, (err) => {
-      response.send('Something went wrong. '+err.message);
-    });
+  const sgMail = require('@sendgrid/mail');
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; // TODO: Hack to run without certificate error
+  const msg = {
+    to: request.params.to.split(','), // Provide an array of comma separated email addresses
+    cc: request.params.cc.split(','), // Provide an array of comma separated email addresses
+    bcc: request.params.bcc.split(','), // // Provide an array of comma separated email addresses
+    from: request.params.from,
+    subject: request.params.subject,
+    text: request.params.content,
+    html: request.params.content,
+  };
+  sgMail.send(msg).then((body, error) => {
+    response.json(body);
+  }, (error) => {
+    response.send(`Something went wrong. ${error.message}`);
+  });
 
 };
