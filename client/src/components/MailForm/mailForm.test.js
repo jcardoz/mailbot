4133461c194bfab4100fa8048feeab17a377a18e
Jonathan Cardoz <jcardoz@deloitte.com>
2019-11-29 03:33:18 +0530

    add: simple UTCs for the MailForm component

--- client/src/components/MailForm/mailForm.test.js
+++ client/src/components/MailForm/mailForm.test.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import MailForm from './mailForm';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MailForm />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('MailForm component', () => {
+  it('contains a heading', () => {
+    const wrapper = shallow(<MailForm />);
+    const text = wrapper.find('h3').text();
+    expect(text).toEqual('Mailbot');
+  });
+});
+
+describe('MailForm component', () => {
+  it('contains a form', () => {
+    const wrapper = shallow(<MailForm />);
+    const name = wrapper.find('form').prop('name');
+    expect(name).toEqual('mailForm');
+  });
 });
