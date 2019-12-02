11a18368a509d1bc8fabc73843aa5a7d42f688fa
Jonathan Cardoz <jcardoz@deloitte.com>
2019-12-03 01:51:14 +0530

    fix: removed unwanted route

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,10 +4,6 @@ var router = express.Router();
 const mailGunController = require('../controllers/mailGunController');
 const sendGridController = require('../controllers/sendGridController');
 
-router.get('/', (req, res, next) => {
-  res.send('hello');
-});
-
 // Mailgun
 router.post('/mailgun', mailGunController.mailHandler);
 
