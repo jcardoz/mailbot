f99fd7016f9723e065f93eeb3711403a0a734251
Jonathan Cardoz <jcardoz@deloitte.com>
2019-12-04 20:06:07 +0530

    Revert "add: API base path to be handled from env variables"
    This reverts commit eda87cae8509da3f26292c7a8e46057545121c6d.

--- client/src/helpers/serviceCallHelper.js
+++ client/src/helpers/serviceCallHelper.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASEURL = process.env.Client_API_BasePath || 'http://localhost:7000';
+const BASEURL = 'http://localhost:7000'; // TODO: Make configurable based on local/prod environment
 
 const makePOSTcall = (APIEndpoint, messageInformation, successHandler, errorHandler) => {
 
